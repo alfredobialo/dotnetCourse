5ee282106785f59f77dfef870d04015d5501ae07
Alfred Obialo <alfredcsdinc@gmail.com>
2022-07-12 13:52:57 +0100

    Do..Loop, for..loop and basic app Logic

--- Module1/Program.cs
+++ Module1/Program.cs
@@ -9,16 +9,51 @@ Console.WriteLine("Hello, World!");
 int[] peoplesAge = new int[5] { 30, 25, 42, 18, 32 };  // Array declaration and assignment (initialization)
 
 // requirement for ages less than 30 , multiply by 2, then Print all results
-
+WriteLine("FOREACH LOOP ");
 foreach (int i in peoplesAge)
 {
     int y = i;
     if (i < 30)
-    {
         y =  i * 2;
-    }
+
+    WriteLine($"Age is : {y}");
+}
+
+
+// FOR LOOP
+WriteLine("FOR LOOP ");
+for (int i = 0; i < peoplesAge.Length; i++)
+{
+    int y = peoplesAge[i];
+
+    if (peoplesAge[i] < 30)
+        y = peoplesAge[i] * 2;
     
     WriteLine($"Age is : {y}");
 }
 
+//DO LOOP
+WriteLine("DO..WHILE LOOP ");
+int a = 0, b = 0;
+do
+{
+    b = peoplesAge[a];
+    if (peoplesAge[a] < 30)
+        b = peoplesAge[a] * 2;
+    WriteLine($"Age is : {b}");
+    a += 1;
+} while (a < peoplesAge.Length);
+
+int counter = 0;
+do
+{
+    ++counter;
+    WriteLine($"Processing Pending Background Transactions {counter}");
+    await Task.Delay(500);
+
+} while (whileWeStillHavePendingTransactionToProcess());
+
+
+
+bool whileWeStillHavePendingTransactionToProcess() =>  counter < 50;
 
