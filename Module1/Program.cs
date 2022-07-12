4ff89d096e8bb8b20e920fd3f88ee6b47bd3eef4
Alfred Obialo <alfredcsdinc@gmail.com>
2022-07-12 13:10:54 +0100

    Foreach each loop example

---
+++ Module1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+
+Console.WriteLine("Hello, World!");
+
+// foreach, for, do, while, goto
+// , if, if..else, switch
+
+int[] peoplesAge = new int[5] { 30, 25, 42, 18, 32 };  // Array declaration and assignment (initialization)
+
+// requirement for ages less than 30 , multiply by 2, then Print all results
+
+foreach (int i in peoplesAge)
+{
+    int y = i;
+    if (i < 30)
+    {
+        y =  i * 2;
+    }
+    
+    WriteLine($"Age is : {y}");
+}
+
+
