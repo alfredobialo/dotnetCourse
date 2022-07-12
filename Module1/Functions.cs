d768ad3884f5c6a2d0d058365b0ed06ececbd5ab
Alfred Obialo <alfredcsdinc@gmail.com>
2022-07-12 15:39:37 +0100

    function

---
+++ Module1/Functions.cs
@@ -0,0 +1,15 @@
+﻿namespace Module1;
+
+public static class Functions
+{
+    public static string ReverseString(string str)
+    {
+        string result = "";
+        foreach (var s in str)  // Hello
+        {
+            result = s + result; // olleH
+        }
+        
+        return result;
+    }
+}
--- Module1/Program.cs
+++ Module1/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Module1;
 using static System.Console;
 
 Console.WriteLine("Hello, World!");
@@ -6,7 +8,7 @@ Console.WriteLine("Hello, World!");
 // foreach, for, do, while, goto
 // , if, if..else, switch
 
-int[] peoplesAge = new int[5] { 30, 25, 42, 18, 32 };  // Array declaration and assignment (initialization)
+int[] peoplesAge = new int[5] { 30, 25, 42, 18, 32 }; // Array declaration and assignment (initialization)
 
 // requirement for ages less than 30 , multiply by 2, then Print all results
 WriteLine("FOREACH LOOP ");
@@ -14,7 +16,7 @@ foreach (int i in peoplesAge)
 {
     int y = i;
     if (i < 30)
-        y =  i * 2;
+        y = i * 2;
 
     WriteLine($"Age is : {y}");
 }
@@ -28,7 +30,7 @@ for (int i = 0; i < peoplesAge.Length; i++)
 
     if (peoplesAge[i] < 30)
         y = peoplesAge[i] * 2;
-    
+
     WriteLine($"Age is : {y}");
 }
 
@@ -38,8 +40,8 @@ int a = 0, b = 0;
 do
 {
     b = peoplesAge[a];
-    if (b < 30)  b *= 2; 
-    
+    if (b < 30) b *= 2;
+
     WriteLine($"Age is : {b}");
     a += 1;
 } while (a < peoplesAge.Length);
@@ -50,12 +52,11 @@ do
     ++counter;
     WriteLine($"Processing Pending Background Transactions {counter}");
     await Task.Delay(500);
-
 } while (whileWeStillHavePendingTransactionToProcess());
 
 
-
-bool whileWeStillHavePendingTransactionToProcess() =>  counter < 2;
+bool whileWeStillHavePendingTransactionToProcess() =>
+    counter < 2;
 
 //While LOOP
 WriteLine("WHILE LOOP ");
@@ -63,13 +64,14 @@ int c = 0, d = 0;
 while (c < peoplesAge.Length) // while this condition is valid
 {
     d = peoplesAge[c];
-    if (d < 30) d *=  2;
-   
+    if (d < 30) d *= 2;
+
     WriteLine($"Age Is  : {d}");
     c += 1;
 }
 
-var x = 10 % 3; // 1
+var xyz = 10 % 3; // 1
+
 /*
  *      10 Mod 3  = 1
  *      (10 - (10 % 3)) / 3
@@ -78,7 +80,19 @@ var x = 10 % 3; // 1
  *
  * 
  */
-int getQuotient(int upper, int lower) => (upper - upper % lower) / lower; 
-    
-    
-    WriteLine($"Quotient of 10/3 = {getQuotient(10,3)}");
+int getQuotient(int upper, int lower) =>
+    (upper - upper % lower) / lower;
+
+
+WriteLine($"Quotient of 10/3 = {getQuotient(10, 3)}");
+
+//function Usage
+WriteLine("FUNCTION USAGE");
+
+var data = "Hello World";
+string rStr = Functions.ReverseString(data);
+
+WriteLine($"string '{data}' is when reversed will be {rStr}");
+
+WriteLine("Press any Key to Terminate Program > ");
+ReadLine();
